7def3e3a0f6c59e292fb915926b4af1cdf06296d
Naufal Dzaky <naufalyuu62@gmail.com>
2024-05-09 12:54:43 +0700

    initial commit

--- js/script.js
+++ js/script.js
@@ -15,4 +15,33 @@ function validation() {
 } 
 
 
-document.getElementById("send-btn").addEventListener('click', () => validation())
+// Section slide
+let indexslide = 1;
+showSlide(1);
+// triger next slide
+function nextSlide(n) {
+    console.log("next slide " + n);
+    showSlide(indexslide += n);
+}
+
+// slide logic
+function showSlide(index) {
+    let listimage = document.getElementsByClassName('banner-item');
+    console.log(listimage);
+    console.log('banyaknya image: ' + listimage.length);
+    // reset slide index
+    if(index > listimage.length) indexslide =1;
+    // hide all image
+    let i =0;
+    while(i < listimage.length){
+        listimage[i].style.display ="none";
+        i++;
+    }
+    // show only one image
+    listimage[indexslide - 1].style.display="block";
+    console.log('showslide ' + index);
+}
+// auto slide
+setInterval(() => nextSlide(1), 2000);
+
+document.getElementById("send-btn").addEventListener('click', () => nextSlide(1))
